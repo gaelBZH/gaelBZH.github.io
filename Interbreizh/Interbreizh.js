983f86bc500362a09a7555ef7346829b966c1766
Gaël <89847466+gaelBZH@users.noreply.github.com>
2023-07-07 11:44:12 +0200

    Interbreizh 📰
    Extension Interbreizh 📰

---
+++ Interbreizh/Interbreizh.js
@@ -0,0 +1,12 @@
+function Select(item)
+{
+    if(item.style.color=='black')
+    {
+        item.style.color='white';
+        item.style.backgroundColor='#193f6c'
+    }
+    else{
+        item.style.color='black';
+        item.style.backgroundColor='#f8f8f8'
+    }
+}
