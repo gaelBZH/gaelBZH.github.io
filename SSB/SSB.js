ec86189a0c2cde6450ed298e7d4619ae6593e073
Gaël <89847466+gaelBZH@users.noreply.github.com>
2023-08-09 19:34:22 +0200

    SSB ☘
    Extension SSB ☘

---
+++ SSB/SSB.js
@@ -0,0 +1,45 @@
+function passwordshow()
+{
+    let x = document.getElementById("password");
+    if (x.type === "password")
+    {
+      x.type = "text";
+    }
+    else
+    {
+      x.type = "password";
+    }
+}
+
+function verifiermdp()
+{
+    let x = document.getElementById("password");
+    let y = document.getElementById("boutonacces");
+    let a = ["1","2","3","4","5","6","77","888","9"];
+    let z = a[0]+a[8]+a[2]+a[8]+a[4]+a[5]+a[3]+a[4]+a[1]+a[1];
+    x.style.color = "white";
+    if (x.value == z)
+    {
+        x.style.backgroundColor = "green";
+        y.disabled = false;
+        x.readOnly = true;
+        return true
+    }
+    else
+    {
+      x.style.backgroundColor = "red";
+      y.disabled = true;
+      return false
+    }
+}
+
+function afficherpage(u){
+    if (verifiermdp()||u==29)
+    {
+        document.getElementById("verrouille").open = true;
+        document.getElementsByClassName("Logo")[0].src = "SSB/BlasonSSB2.png";
+        document.getElementById("TitrePrincipal").textContent = "Bureau du SSB"
+        document.getElementsByTagName("h2")[0].textContent="Meus honor vocatur fides";
+        document.getElementById("Sommaire").style.visibility="visible";
+    }
+}
