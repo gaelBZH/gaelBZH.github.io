5b561a437b9fe874b1a69cae691a486f0e43f3ea
Gaël <89847466+gaelBZH@users.noreply.github.com>
2023-08-16 11:41:20 +0200

    Interbreizh 📰
    Extension Interbreizh 📰

--- Interbreizh/Interbreizh.js
+++ Interbreizh/Interbreizh.js
@@ -16,5 +16,22 @@ function StopPub()
     document.getElementById('pub').style.display='none'
 }
 
-document.getElementById('close').addEventListener('click', StopPub);
-document.getElementById("year").innerHTML=(new Date().getFullYear());
+document.getElementById('close').addEventListener('click', StopPub); // Stop Pub
+document.getElementById("year").innerHTML=(new Date().getFullYear()); // Année
+
+// Barre de Recherche
+const articles = document.querySelectorAll('.Article');
+
+document.getElementById('BarreDeRecherche').addEventListener('input', () => {
+    const searchTerms = normalizeSearchTerms(document.getElementById('BarreDeRecherche').value);
+
+    articles.forEach(article => {
+        const title = normalizeText(article.querySelector('h3').textContent);
+        const matchingTerms = searchTerms.filter(term => title.includes(term));
+        article.style.display = matchingTerms.length * 2 >= searchTerms.length ? 'block' : 'none';
+    });
+});
+
+const normalizeText = text => text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+const normalizeSearchTerms = terms => terms.toLowerCase().split(/\s+/).map(normalizeText);
+
