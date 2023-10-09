a1a43e847f7d07c79cb970b7ae115655429b317e
Gaël <89847466+gaelBZH@users.noreply.github.com>
2023-10-09 21:22:42 +0200

    Update Interbreizh.js

--- Interbreizh/Interbreizh.js
+++ Interbreizh/Interbreizh.js
@@ -30,7 +30,10 @@ document.getElementById("year").innerHTML=(new Date().getFullYear());
 document.getElementById('BarreDeRecherche').addEventListener('input', () => {document.querySelectorAll('.Article').forEach(article => {article.style.display = normalizeSearchTerms(document.getElementById('BarreDeRecherche').value).filter(term => normalizeText(article.querySelector('h3').textContent).includes(term)).length * 2 >= normalizeSearchTerms(document.getElementById('BarreDeRecherche').value).length ? 'block' : 'none';});});
 
 // Couleur Onglet
-changerCouleurOnglet(document.documentElement.getPropertyValue('--bleuinterbreizhTEST'));
+var style = getComputedStyle(document.body)
+console.log( style.getPropertyValue('--bleuinterbreizh') )
+changerCouleurOnglet(style.getPropertyValue('--bleuinterbreizh'));
+
 
 const normalizeText = text => text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 const normalizeSearchTerms = terms => terms.toLowerCase().split(/\s+/).map(normalizeText);
