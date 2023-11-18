eb8c84b7bcde3f9c297a8e62ed9bf4ad5fe043e5
Gaël <89847466+gaelBZH@users.noreply.github.com>
2023-11-18 15:16:04 +0100

    Maintenance

---
+++ Interbreizh/Article.js
@@ -0,0 +1,21 @@
+function DarkMode()
+{
+    let blanc = '#e5e5e5';
+    let noir = '#202121';
+    document.querySelector(':root').style.setProperty('--bleuinterbreizh', '#04162d');
+    document.querySelector(':root').style.setProperty('--bleuinterbreizhclair', '#0e233d');
+    document.querySelector(':root').style.setProperty('--texte', blanc);
+    document.querySelector(':root').style.setProperty('--highlightedtexte', blanc);
+    document.body.style.backgroundColor = noir;
+    document.querySelector("#Sujets").style.color=blanc;
+}
+function changerCouleurOnglet(couleur) {
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', couleur);
+}
+
+
+var NumeroPage = window.location.pathname.split("/").pop().match(/\d+/)
+document.title = "Interbreizh - Article " + NumeroPage;
+
+changerCouleurOnglet(getComputedStyle(document.documentElement).getPropertyValue('--bleuinterbreizh').trim());
+document.getElementById("year").innerHTML=(new Date().getFullYear());
