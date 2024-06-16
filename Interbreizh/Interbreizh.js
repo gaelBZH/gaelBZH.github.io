e2aec1599e4fd1543e2b4633f0e04ceecd9c2dd3
Gaël <89847466+gaelBZH@users.noreply.github.com>
2024-06-16 23:24:45 +0200

    Interbreizh 📰
    Extension Interbreizh 📰

---
+++ Interbreizh/Interbreizh.js
@@ -0,0 +1,37 @@
+function Select(item)
+{
+    if(item.style.color=='black')
+    {
+        item.style.color='white';
+        item.style.backgroundColor=getComputedStyle(item).getPropertyValue('--bleuinterbreizh')
+    }
+    else{
+        item.style.color='black';
+        item.style.backgroundColor='#f8f8f8'
+    }
+}
+
+function StopPub()
+{
+    document.getElementById('pub').style.display='none'
+}
+
+function changerCouleurOnglet(couleur) {
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', couleur);
+}
+
+// Stop Pub
+document.getElementById('close').addEventListener('click', StopPub);
+
+// Année
+document.getElementById("year").innerHTML=(new Date().getFullYear());
+
+// Barre de Recherche
+document.getElementById('BarreDeRecherche').addEventListener('input', () => {document.querySelectorAll('.Article').forEach(article => {article.style.display = normalizeSearchTerms(document.getElementById('BarreDeRecherche').value).filter(term => normalizeText(article.querySelector('h3').textContent).includes(term)).length * 2 >= normalizeSearchTerms(document.getElementById('BarreDeRecherche').value).length ? 'block' : 'none';});});
+
+// Couleur Onglet
+changerCouleurOnglet(getComputedStyle(document.documentElement).getPropertyValue('--bleuinterbreizh').trim());
+
+const normalizeText = text => text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+const normalizeSearchTerms = terms => terms.toLowerCase().split(/\s+/).map(normalizeText);
+
