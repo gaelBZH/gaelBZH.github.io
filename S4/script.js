26be593d3746e7b4061a128a8ad2cfc6a06acde3
gaelBZH <gaelkerninon3110@gmail.com>
2026-04-16 14:28:53 +0200

    Change Colors to Hexadecimal

--- S4/script.js
+++ S4/script.js
@@ -41,7 +41,7 @@ const clamp = (val) => Math.min(Math.max(val, 0), 100);
             else if (pourcent >= 97)  couleur = "#00562b";
             else if (pourcent >= 95)  couleur = "#008040"
             else if (pourcent >= 92)  couleur = "#00a300";
-            else if (pourcent >= 90)  couleur = "green";
+            else if (pourcent >= 90)  couleur = "#008000"; // green
             else if (pourcent >= 86)  couleur = "#54db54";
             else if (pourcent >= 82)  couleur = "#8EF571"
             else if (pourcent >= 75)  couleur = "#BEF571"
@@ -49,19 +49,18 @@ const clamp = (val) => Math.min(Math.max(val, 0), 100);
             else if (pourcent >= 62)  couleur = "#D8F571"
             else if (pourcent >= 56)  couleur = "#EAF571"
             else if (pourcent >= 53)  couleur = "#f5f571";
-            else if (pourcent >= 50)  couleur = "yellow";
+            else if (pourcent >= 50)  couleur = "#ffff00"; // yellow
             else if (pourcent >= 42)  couleur = "#FFD700"
             else if (pourcent >= 38)  couleur = "#ffbf00";
-            else if (pourcent >= 35)  couleur = "orange";
+            else if (pourcent >= 35)  couleur = "#ffa500"; // orange
             else if (pourcent >= 30)  couleur = "#ff8132";
             else if (pourcent >= 25)  couleur = "#FF6347"
             else if (pourcent >= 20)  couleur = "#ff5331";
-            else if (pourcent >= 15)  couleur = "orangered";
-            else if (pourcent >= 10)  couleur = "red";
+            else if (pourcent >= 15)  couleur = "#ff4500"; // orangered
+            else if (pourcent >= 10)  couleur = "#ff0000"; // red
             else if (pourcent >= 7)   couleur = "#B22222"
-            else if (pourcent >= 5)   couleur = "darkred";
+            else if (pourcent >= 5)   couleur = "#8b0000"; // darkred
             else if (pourcent >= 2)   couleur = "#4B0000"
-
             barre.style.backgroundColor = couleur;
         }
 
